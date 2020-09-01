6148f0c9ca0de5c7bf98c35f633578cea1f0b2e6
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2020-09-01 18:39:13 -0300

    Service layer, component registration

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,21 +1,41 @@
 package com.example.demo.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
 
-@RestController			//Para definirmos que essa classe é um recurso web e que é implementada por controlador rest.
-@RequestMapping(value ="/users")    //Nomeando o recurso. 			
+@RestController													//Para definirmos que essa classe é um recurso web e que é implementada por controlador rest.
+@RequestMapping(value ="/users")    							//Nomeando o recurso. 			
 public class UserResource 
 {
-	//O ResponseEntity<T> é um tipo específico do spring para requisições web
-	@GetMapping 	//Para indicar que esse é um método que responde a requisição do tipo get http
-	public ResponseEntity<User> findAll() //Método que irá retornar os usuários
+	@Autowired
+	private UserService service; 
+	
+	
+	
+	
+	
+																//O ResponseEntity<T> é um tipo específico do spring para requisições web
+	@GetMapping 												//Para indicar que esse é um método que responde a requisição do tipo get http
+	public ResponseEntity<List<User>> findAll() 				//Método que irá retornar os usuários
 	{
-		User u = new User(1L, "João", "joao@gmail.com", "14 9985555", "123456");
-		return ResponseEntity.ok().body(u); 
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list); 
 	}
+	
+	@GetMapping(value ="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id)
+	{
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
