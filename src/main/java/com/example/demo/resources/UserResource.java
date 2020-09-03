67031b6a2e4a15a976b2cc574f103546c5e09e8a
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2020-09-03 18:52:51 -0300

    User insert

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.example.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.entities.User;
 import com.example.demo.services.UserService;
@@ -18,10 +22,6 @@ public class UserResource
 {
 	@Autowired
 	private UserService service; 
-	
-	
-	
-	
 	
 																//O ResponseEntity<T> é um tipo específico do spring para requisições web
 	@GetMapping 												//Para indicar que esse é um método que responde a requisição do tipo get http
@@ -38,4 +38,11 @@ public class UserResource
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping 												//Essa anotação do spring ela realiza um pré-processamento na compilação do meu controlador, defindindo que esse método vai receber um método post do http
+	public ResponseEntity<User> insert(@RequestBody User obj)
+	{
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
